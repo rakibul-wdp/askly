263e0618d53ab815c30820da5dcfede4bf0dda5c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-24 04:05:00 +0600

    create team section of about

--- components/about/Team.js
+++ components/about/Team.js
@@ -1,11 +1,35 @@
+import { teamMembers } from '@/mockData/teamMembers';
+import Image from 'next/image';
 import React from 'react'
 
 const Team = () => {
   return (
-    <div style={{
-      background: `rgba(132, 33, 192, 0.1) 0%
-      rgba(132, 33, 192, 0.1) 100%`,
-    }}>Team</div>
+    <section className='flex justify-between gap-10 pt-24 pl-32' style={{
+      background: `linear-gradient(90deg, rgba(132, 33, 192, 0.1) 0%, rgba(132, 33, 192, 0.1) 100%);`,
+    }}>
+      <div>
+        <h2 className='text-3xl text-primary font-bold mb-5 -mt-14'>Our Team Members</h2>
+        <p>Our team is made up of talented individuals from different backgrounds and with diverse skill sets. We believe that this diversity is what makes us strong and enables us to tackle complex challenges with creativity and innovation.</p>
+        <p className='my-5'>Our team is committed to continuous learning and growth. We stay up-to-date with the latest technologies and industry trends, and we encourage each other to pursue personal and professional development.</p>
+        <p>We are proud of the work we do and the impact it has on our clients and their users. Thank you for being a part of our team and for your contribution to our shared mission.</p>
+      </div>
+      <div>
+        {
+          teamMembers.map((member) => (
+            <div className='w-[70%] flex justify-between rounded-lg mb-5' style={{ background: "rgba(132, 33, 192, 0.1)" }} key={member.id}>
+              <Image src={member.img} width={100} height={100} alt='team member' />
+              <div className='px-5'>
+                <div className='flex items-center justify-between mb-3'>
+                  <h4>{member.name}</h4>
+                  <p>{member.designation}</p>
+                </div>
+                <p className='text-sm'>{member.description}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ mockData/teamMembers.js
@@ -0,0 +1,32 @@
+import TeamMemberImg from "../public/assets/images/about/teamMember.svg";
+
+export const teamMembers = [
+  {
+    id: 1,
+    img: TeamMemberImg,
+    name: "Nour eldin",
+    designation: "Product designer",
+    description: "With over 20 years of experience in the tech industry, Nour brings a wealth of knowledge and expertise to Askly. As our Product designer, Nour is passionate about helping individuals and businesses gain valuable insights from their data."
+  },
+  {
+    id: 2,
+    img: TeamMemberImg,
+    name: "Nour eldin",
+    designation: "Product designer",
+    description: "With over 20 years of experience in the tech industry, Nour brings a wealth of knowledge and expertise to Askly. As our Product designer, Nour is passionate about helping individuals and businesses gain valuable insights from their data."
+  },
+  {
+    id: 3,
+    img: TeamMemberImg,
+    name: "Nour eldin",
+    designation: "Product designer",
+    description: "With over 20 years of experience in the tech industry, Nour brings a wealth of knowledge and expertise to Askly. As our Product designer, Nour is passionate about helping individuals and businesses gain valuable insights from their data."
+  },
+  {
+    id: 4,
+    img: TeamMemberImg,
+    name: "Nour eldin",
+    designation: "Product designer",
+    description: "With over 20 years of experience in the tech industry, Nour brings a wealth of knowledge and expertise to Askly. As our Product designer, Nour is passionate about helping individuals and businesses gain valuable insights from their data."
+  }
+]
