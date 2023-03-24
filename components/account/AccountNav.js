9b12245584fd0a1fbe0010922043c0d0f77287fb
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-25 04:05:00 +0600

    responsive account page navbar

--- components/account/AccountNav.js
+++ components/account/AccountNav.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
+import AsklyLogo from "../../public/assets/images/common/asklyLogo.png";
+import Image from 'next/image';
 
 const AccountNav = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar pt-8 md:pt-10">
       <div className="navbar-start">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <Image src={AsklyLogo} width={250} height={250} alt='askly logo' />
       </div>
       <div className="navbar-end">
-        <a className="btn btn-primary">Button</a>
-        <a className="btn btn-outline btn-primary">Button</a>
+        <button className='btn btn-primary normal-case w-[7rem] md:btn-wide'>Dashboard</button>
+        <button className='btn btn-primary btn-outline normal-case w-[6rem] md:w-[8rem] ml-4'>Sign out</button>
       </div>
     </div>
   )
