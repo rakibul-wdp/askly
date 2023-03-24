d0ffeddb202aab556ff7c471f7e484a3f9c586ea
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-24 07:05:00 +0600

    responsive help page with a issue

--- components/help/Contact.js
+++ components/help/Contact.js
@@ -4,18 +4,18 @@ import Image from 'next/image';
 
 const Contact = () => {
   return (
-    <section className='flex items-center justify-between px-24 pb-14 pt-5'>
-      <div className='w-[48%]'>
+    <section className='flex flex-col-reverse md:flex-row items-center justify-between md:gap-10 lg:gap-24 pb-14 pt-10'>
+      <div className='md:w-[50%] lg:w-[75%] xl:w-[50%]'>
         <form>
           <input type="text" placeholder="Full Name" className="input input-bordered input-primary w-full mb-5" style={{ background: "rgba(255, 127, 127, 0.02)" }} />
           <input type="email" placeholder="Email" className="input input-bordered input-primary w-full mb-5" style={{ background: "rgba(255, 127, 127, 0.02)" }} />
           <input type="number" placeholder="Phone Number" className="input input-bordered input-primary w-full mb-5" style={{ background: "rgba(255, 127, 127, 0.02)" }} />
           <textarea className="textarea textarea-bordered textarea-primary w-full" placeholder="Write your message here" style={{ background: "rgba(255, 127, 127, 0.02)" }}></textarea>
-          <input className='btn btn-primary normal-case w-full mt-3 mb-5' type="submit" value="Send" />
+          <input className='btn btn-primary normal-case w-full mt-3' type="submit" value="Send" />
         </form>
       </div>
-      <div className='w-[42%]'>
-        <p>At Askly, we're committed to providing you with an exceptional experience using our platform. If you have any questions, feedback, or need assistance with our product, our friendly and knowledgeable support team is available 24/7 to assist you. Please don't hesitate to contact us with any inquiries. We appreciate your input and look forward to helping you get the most out of Askly</p>
+      <div className='md:w-[50%] lg:w-[75%] xl:w-[50%] mb-10 md:mb-0'>
+        <p className='mb-2'>At Askly, we're committed to providing you with an exceptional experience using our platform. If you have any questions, feedback, or need assistance with our product, our friendly and knowledgeable support team is available 24/7 to assist you. Please don't hesitate to contact us with any inquiries. We appreciate your input and look forward to helping you get the most out of Askly</p>
         <Image src={ContactImg} width={700} height={200} alt='contact with us' />
       </div>
     </section>
--- components/help/FAQ.js
+++ components/help/FAQ.js
@@ -9,18 +9,18 @@ import { FAQHelp } from '@/mockData/FAQHelp';
 
 const FAQ = () => {
   return (
-    <section className='h-[80vh]'>
+    <section className=''>
       <Navbar />
       <div className='pt-32'>
-        <h2 className='text-3xl font-bold text-center'>Frequently Asked Questions</h2>
-        <div className="w-full px-4 pt-16">
-          <div className="mx-auto w-full rounded-2xl p-2">
+        <h2 className='text-3xl md:text-4xl font-bold text-center'>Frequently Asked Questions</h2>
+        <div className="w-full pt-12">
+          <div className="mx-auto w-full rounded-2xl py-2">
             {
               FAQHelp.map((faq) => (
                 <Disclosure as={`${faq.id !== 1 && "div"}`} className={`${faq.id !== 1 && "mt-2"}`} key={faq.id}>
                   {({ open }) => (
                     <>
-                      <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg bg-purple-100 px-4 py-3 text-lg text-left font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                         <span>{faq.question}</span>
                         <Image className={`${open && 'rotate-180 transform'}`} src={ArrowIcon} width={5} height={5} alt='arrow icon' />
                       </Disclosure.Button>
