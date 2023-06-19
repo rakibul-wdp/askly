759f0ec7ef22e5a4a9b4e84d910ea8e2b98ad05d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-19 07:58:08 +0600

    create few page with boilerplate

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button class="btn btn-bg-1">Button</button>
+      <button className="btn btn-bg-1">Button</button>
       <h1 className='text-3xl font-bold text-neutral'>Hello World</h1>
     </main>
   )
