b0b813d07951247e598ae850b6c25c12fb65459c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-19 09:11:19 +0600

    create mock data

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navbar from '@/components/common/Navbar';
 import HomeHero from '@/components/home/HomeHero';
+import HomeMain from '@/components/home/HomeMain';
 
 export default function Home() {
   return (
     <main className='relative'>
       <Navbar />
       <HomeHero />
+      <HomeMain />
     </main>
   )
 }
---
+++ mockData/homeMain.js
@@ -0,0 +1,63 @@
+import ImportDataImg from "../public/assets/images/home/importData.svg";
+import QuestionImg from "../public/assets/images/home/question.svg";
+import AnswersImg from "../public/assets/images/home/answers.svg";
+
+export const homeMain = [
+  {
+    id: 1,
+    title: "Import your data",
+    description: [
+      {
+        id: 1,
+        listItem: "Vector search powered by deep learning-based LLMs enables semantic understanding for natural language queries"
+      },
+      {
+        id: 2,
+        listItem: "Glean continuously trains on your company’s unique language and context to improve search relevance. No need for manual fine-tuning."
+      },
+      {
+        id: 3,
+        listItem: "Generative AI grounded in your company’s own trusted knowledge model delivers real answers."
+      }
+    ],
+    img: ImportDataImg
+  },
+  {
+    id: 2,
+    title: "Write in the query your question",
+    description: [
+      {
+        id: 1,
+        listItem: "Vector search powered by deep learning-based LLMs enables semantic understanding for natural language queries"
+      },
+      {
+        id: 2,
+        listItem: "Glean continuously trains on your company’s unique language and context to improve search relevance. No need for manual fine-tuning."
+      },
+      {
+        id: 3,
+        listItem: "Generative AI grounded in your company’s own trusted knowledge model delivers real answers."
+      }
+    ],
+    img: QuestionImg
+  },
+  {
+    id: 3,
+    title: "Askly will respond with the best answers ",
+    description: [
+      {
+        id: 1,
+        listItem: "Vector search powered by deep learning-based LLMs enables semantic understanding for natural language queries"
+      },
+      {
+        id: 2,
+        listItem: "Glean continuously trains on your company’s unique language and context to improve search relevance. No need for manual fine-tuning."
+      },
+      {
+        id: 3,
+        listItem: "Generative AI grounded in your company’s own trusted knowledge model delivers real answers."
+      }
+    ],
+    img: AnswersImg
+  }
+]
