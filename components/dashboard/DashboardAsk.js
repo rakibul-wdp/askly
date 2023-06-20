7e8975f3f32c31187a9b472d7a94d43b52ce8854
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-21 05:22:34 +0600

    create a modal structure

--- components/dashboard/DashboardAsk.js
+++ components/dashboard/DashboardAsk.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FileSelectModal from './FileSelectModal'
 
 const DashboardAsk = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
   return (
     <section>
       <div className='text-end my-14 mr-10'>
         <button className='btn btn-primary btn-outline'>Saved Session</button>
-        <button className='btn btn-primary normal-case ml-5'>Select Files</button>
+        <button onClick={openModal} className='btn btn-primary normal-case ml-5'>Select Files</button>
       </div>
       <h3 className='text-3xl font-medium text-center mb-5'>Select Files to Start a Session</h3>
       <input type="text" placeholder="Full Name" className="input input-bordered input-primary w-[80%] mx-32" style={{ background: "rgba(255, 127, 127, 0.02)" }} />
+
+      <FileSelectModal isOpen={isOpen} closeModal={closeModal} />
     </section>
   )
 }
