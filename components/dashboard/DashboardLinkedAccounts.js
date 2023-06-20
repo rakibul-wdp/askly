571cc26e92a2d1e766982dba6ff26869f59abeec
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-20 20:00:58 +0600

    create dashboard linked account component

--- components/dashboard/DashboardLinkedAccounts.js
+++ components/dashboard/DashboardLinkedAccounts.js
@@ -1,8 +1,24 @@
+import { dashboardLinkedAccounts } from '@/mockData/dashboardLinedAccounts';
+import Image from 'next/image';
 import React from 'react'
 
 const DashboardLinkedAccounts = () => {
   return (
-    <div>DashboardLinkedAccounts</div>
+    <section>
+      <h1 className='text-5xl font-bold text-center py-14' style={{ color: "rgba(0, 0, 0, 0.6)" }}>Coming Soon</h1>
+      <h5 className='text-xl font-medium mb-4'>My Accounts</h5>
+      <div className='grid grid-cols-5'>
+        {
+          dashboardLinkedAccounts.map((account) => (
+            <div className='w-full' key={account.id}>
+              <Image className='rounded-2xl' src={account.img} width={250} height={250} alt='linked accounts' />
+              {account.id === 5 ? <button className='btn btn-neutral normal-case w-[80%] my-3'>Sign out</button> : <button className='btn btn-neutral normal-case w-[80%] mt-3'>Sign in</button>}
+              {account.id === 5 && <button className='btn btn-neutral btn-outline normal-case w-[80%]'>Remove</button>}
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ mockData/dashboardLinedAccounts.js
@@ -0,0 +1,28 @@
+import SlackImg from "../public/assets/images/dashboard/slack.svg";
+import DriveImg from "../public/assets/images/dashboard/drive.svg";
+import DropboxImg from "../public/assets/images/dashboard/dropbox.svg";
+import NotionImg from "../public/assets/images/dashboard/notion.svg";
+import MailImg from "../public/assets/images/dashboard/mail.svg";
+
+export const dashboardLinkedAccounts = [
+  {
+    id: 1,
+    img: SlackImg
+  },
+  {
+    id: 2,
+    img: DriveImg
+  },
+  {
+    id: 3,
+    img: DropboxImg
+  },
+  {
+    id: 4,
+    img: NotionImg
+  },
+  {
+    id: 5,
+    img: MailImg
+  },
+]
