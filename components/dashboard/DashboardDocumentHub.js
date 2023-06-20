ed8b883a5777bed605f64ce7cbab9711d99696ac
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-20 20:34:15 +0600

    create documents mock data

--- components/dashboard/DashboardDocumentHub.js
+++ components/dashboard/DashboardDocumentHub.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const DashboardDocumentHub = () => {
   return (
-    <div>DashboardDocumentHub</div>
+    <section>
+      <div className='text-end my-14 mr-10'>
+        <button className='btn btn-outline border-dashed'>Drag your files here</button>
+        <button className='btn btn-primary normal-case mx-3'>Upload</button>
+        <button className='btn bg-base-100'>Delete</button>
+      </div>
+      <h4 className='text-xl text-primary font-medium'>My Documents</h4>
+    </section>
   )
 }
 
---
+++ mockData/dashboardDocuments.js
@@ -0,0 +1,32 @@
+export const dashboardDocuments = [
+  {
+    id: 1,
+    checked: true,
+    filetTitle: "File title"
+  },
+  {
+    id: 2,
+    checked: true,
+    filetTitle: "File title"
+  },
+  {
+    id: 3,
+    checked: false,
+    filetTitle: "File title"
+  },
+  {
+    id: 4,
+    checked: false,
+    filetTitle: "File title"
+  },
+  {
+    id: 5,
+    checked: false,
+    filetTitle: "File title"
+  },
+  {
+    id: 6,
+    checked: false,
+    filetTitle: "File title"
+  },
+]
