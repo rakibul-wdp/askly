43e46dae7021e5f9523515615c6a412c3da01450
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-20 21:12:12 +0600

    create dashboard document component

--- components/dashboard/DashboardDocumentHub.js
+++ components/dashboard/DashboardDocumentHub.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { dashboardDocuments } from '@/mockData/dashboardDocuments';
+import FileImg from "../../public/assets/images/dashboard/file.svg";
+import Image from 'next/image';
 
 const DashboardDocumentHub = () => {
   return (
@@ -8,7 +11,20 @@ const DashboardDocumentHub = () => {
         <button className='btn btn-primary normal-case mx-3'>Upload</button>
         <button className='btn bg-base-100'>Delete</button>
       </div>
-      <h4 className='text-xl text-primary font-medium'>My Documents</h4>
+      <h4 className='text-xl text-primary font-medium mb-14'>My Documents</h4>
+      <div className='grid grid-cols-6 w-[80%]'>
+        {
+          dashboardDocuments.map((document) => (
+            <div className='flex justify-center' key={document.id}>
+              <input type="checkbox" checked={`${document.checked ? "checked" : ""}`} className="checkbox checkbox-primary -mt-6 mr-5" />
+              <div>
+                <Image src={FileImg} width={100} height={100} alt='file' />
+                <h5>{document.filetTitle}</h5>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </section>
   )
 }
