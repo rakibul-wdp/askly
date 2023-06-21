6181f22ee3c87bb5ed13ea53788fe086734abb3b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-21 16:27:29 +0600

    login page responsive done without input field

--- app/login/page.js
+++ app/login/page.js
@@ -14,13 +14,15 @@ const Login = () => {
       background: `linear-gradient(134.56deg, rgba(155, 126, 234, 0.2) 10.8%, rgba(255, 127, 127, 0.2) 88.78%)`,
     }}>
       <Navbar />
-      <form onSubmit={handleLoginSubmit} className='absolute bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/2 bg-accent p-5 rounded-lg w-[25%]'>
-        <h2 className='text-xl font-bold mb-3'>Welcome back</h2>
+      <form onSubmit={handleLoginSubmit} className='absolute bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/2 bg-accent p-5 rounded-lg w-[90%] sm:w-[70%] md:w-[50%] lg:w-[45%] xl:w-[40%] 2xl:w[30%]' style={{
+        background: `linear-gradient(134.56deg, rgba(155, 126, 234, 0.1) 10.8%, rgba(255, 127, 127, 0.1) 88.78%)`,
+      }}>
+        <h2 className='text-2xl font-bold mb-3'>Welcome back</h2>
         <input type="email" placeholder="Type here" className="input input-bordered w-full mb-5" />
         <input type="password" placeholder="Type here" className="input input-bordered w-full" />
-        <p className='my-3'>Forget your password?</p>
-        <input className='btn btn-primary normal-case w-full mb-3' type="submit" value="Login" />
-        <p>Don't have an account? <Link href="/signup">Sign up</Link></p>
+        <p className='my-3.5 underline'>Forget your password?</p>
+        <input className='btn btn-primary normal-case w-full mb-3.5' type="submit" value="Login" />
+        <p>Don't have an account? <Link className='underline' href="/signup">Sign up</Link></p>
       </form>
     </section>
   )
