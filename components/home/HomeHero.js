4c1473a93194458be22d9c82ab05b262841c9e9d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-21 14:19:11 +0600

    responsive home hero section

--- components/home/HomeHero.js
+++ components/home/HomeHero.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import PrimaryButton from '../common/PrimaryButton';
 
 const HomeHero = () => {
   return (
     <section className='bg-neutral h-screen'>
-      <div className='absolute bottom-10 right-10 text-right'>
-        <div className='mb-10 text-3xl font-bold'><span className='text-primary'>“Ask more,</span> <span className='bg-gradient-to-b from-purple-600 to-red-600 bg-clip-text text-transparent'>search less”</span></div>
-        <button className='btn btn-primary normal-case'>Get Started</button>
+      <div className='absolute bottom-10 right-0 left-0 md:right-2 lg:right-3 2xl:-right-5 text-center md:text-right max-w-screen-2xl pl-3 sm:px-5 md:px-8 lg:px-14 xl:px-20 2xl:px-24'>
+        <div className='mb-10 text-3xl font-bold'>
+          <span className='text-primary'>“Ask more,</span> <span className='bg-gradient-to-b from-purple-600 to-red-600 bg-clip-text text-transparent'>search less”</span>
+        </div>
+        <PrimaryButton>Get Started</PrimaryButton>
       </div>
     </section>
   )
