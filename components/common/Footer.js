40a3e19fa670b0e7e94a5fb3b8114a347df67630
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-21 15:26:20 +0600

    footer responsive done

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import AsklyLogo from "../../public/assets/images/common/asklyLogo.png";
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="footer items-center px-4 pt-2 pb-10 bg-accent">
-      <div className="items-center grid-flow-col">
-        <h2 className='text-3xl font-bold uppercase'>Logo</h2>
-        <p className='font-medium'>Copyright © 2023 - All right reserved</p>
+    <footer className="flex lg:items-center justify-between pt-2 pb-10 bg-accent max-w-screen-2xl mx-auto px-3 sm:px-5 md:px-8 lg:px-14 xl:px-20 2xl:px-24">
+      <div className="flex flex-col xl:flex-row xl:items-center">
+        <Image src={AsklyLogo} width={250} height={250} lang='askly logo' />
+        <p className='text-xl font-medium mt-2 md:mt-0'>Copyright © 2023 - All right reserved</p>
       </div>
-      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a>About</a>
-        <a>Contact</a>
-        <a>FAQ</a>
-        <a>Privacy Policy</a>
-        <a>Terms of Service</a>
+      <div className="flex flex-col lg:flex-row lg:items-center text-xl">
+        <Link href="/about">About</Link>
+        <Link className='my-2 lg:my-0 lg:mx-5' href="#">Contact</Link>
+        <Link href="#">FAQ</Link>
+        <Link className='my-2 lg:my-0 lg:mx-5' href="/privacypolicy">Privacy Policy</Link>
+        <Link href="/termsofservice">Terms of Service</Link>
       </div>
     </footer>
   )
--- components/home/HomeMain.js
+++ components/home/HomeMain.js
@@ -7,10 +7,10 @@ const HomeMain = () => {
     <div className='bg-[#F2E3EF] pt-10 pb-32'>
       {
         homeMain.map((item) => (
-          <div key={item.id} className={`flex ${item.id === 2 && "flex-row-reverse my-24"} items-center justify-center gap-32 relative`}>
+          <div key={item.id} className={`flex flex-col-reverse ${item.id === 2 ? "md:flex-row-reverse my-24" : "md:flex-row"} items-center justify-center gap-32 relative right-0 left-0 max-w-screen-2xl mx-auto pl-3 sm:px-5 md:px-8 lg:px-14 xl:px-20 2xl:px-24`}>
             <span className="absolute top-0 right-[45rem] z-0 text-[24rem] text-base-100 font-bold">{item.id}</span>
-            <Image src={item.img} height={602} width={624} alt='home main section images' />
-            <div className='w-[30%] relative z-10'>
+            <Image src={item.img} sizes='10vw' height={602} width={624} alt='home main section images' />
+            <div className='w-full sm:w-[90%] md:w-[70%] lg:w-[30%] relative z-10'>
               <h2 className='text-3xl text-primary font-bold mb-5'>{item.title}</h2>
               <ul className='list-disc list-outside ml-5'>
                 {
