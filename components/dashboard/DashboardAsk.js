6b63d794968408fb8c3fa4f26b4df01eb136e45f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-22 20:09:50 +0600

    responsive dashboard ask section

--- components/dashboard/DashboardAsk.js
+++ components/dashboard/DashboardAsk.js
@@ -17,12 +17,14 @@ const DashboardAsk = () => {
   return (
     <>
       {!session ? <section>
-        <div className='text-end my-14 mr-10'>
-          <button onClick={() => setSession(true)} className='btn btn-primary btn-outline'>Saved Session</button>
-          <button onClick={openModal} className='btn btn-primary normal-case ml-5'>Select Files</button>
+        <div className='text-center md:text-end my-14 mr-10'>
+          <button onClick={() => setSession(true)} className='btn btn-primary normal-case btn-outline w-[10rem] md:btn-wide'>Saved Session</button>
+          <button onClick={openModal} className='btn btn-primary normal-case w-[10rem] md:btn-wide ml-5'>Select Files</button>
         </div>
-        <h3 className='text-3xl font-medium text-center mb-5'>Select Files to Start a Session</h3>
-        <input type="text" placeholder="Full Name" className="input input-bordered input-primary w-[80%] mx-32" style={{ background: "rgba(255, 127, 127, 0.02)" }} />
+        <h3 className='text-3xl font-medium text-center mb-5 px-3 sm:px-5 md:px-8'>Select Files to Start a Session</h3>
+        <div className='text-center'>
+          <input type="text" placeholder="Full Name" className="input input-bordered input-primary w-[80%] px-3 sm:px-5 md:px-8" style={{ background: "rgba(255, 127, 127, 0.02)" }} />
+        </div>
 
         <FileSelectModal isOpen={isOpen} closeModal={closeModal} />
       </section> : <DashboardSession />}
