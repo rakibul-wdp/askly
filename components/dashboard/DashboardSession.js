d343b7b07332adc22a1ed2f4a914d9a507642aad
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-22 21:19:03 +0600

    dashboard session button responsive

--- components/dashboard/DashboardSession.js
+++ components/dashboard/DashboardSession.js
@@ -3,10 +3,10 @@ import React from 'react'
 const DashboardSession = () => {
   return (
     <section>
-      <div className='text-center md:text-end my-14 md:mr-10'>
-        <button className='btn btn-primary btn-outline w-[10rem] md:btn-wide normal-case'>Record Session</button>
-        <button className='btn btn-primary btn-outline w-[10rem] md:btn-wide normal-case mx-5'>Share Session</button>
-        <button className='btn btn-primary btn-outline w-[10rem] md:btn-wide normal-case'>End Session</button>
+      <div className='flex flex-col items-center md:items-end justify-center md:justify-end sm:flex-row my-14 md:mr-10'>
+        <button className='btn btn-primary btn-outline btn-wide sm:w-[10rem] xl:w-[16rem] normal-case'>Record Session</button>
+        <button className='btn btn-primary btn-outline btn-wide sm:w-[10rem] xl:w-[16rem] my-5 sm:my-0 normal-case mx-5'>Share Session</button>
+        <button className='btn btn-primary btn-outline btn-wide sm:w-[10rem] xl:w-[16rem] normal-case'>End Session</button>
       </div>
       <div className='w-[98%] mx-auto h-36 rounded-lg' style={{ background: "rgba(132, 33, 192, 0.1)" }}></div>
       <div className='flex items-center justify-between mx-3 mt-5'>
