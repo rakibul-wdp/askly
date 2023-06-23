ed00a623e84d2785a5e9f7cde227e7e1143b92bc
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-23 12:02:43 +0600

    fix login and sign up page

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,16 @@
 "use client";
 
+import React, { useState } from 'react'
 import Navbar from '@/components/common/Navbar';
 import Link from 'next/link';
-import React from 'react'
+import MailIcon from "../../public/assets/icons/email.svg";
+import LockIcon from "../../public/assets/icons/Lock.svg";
+import EyeIcon from "../../public/assets/icons/Eye.svg";
+import Image from 'next/image';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const handleLoginSubmit = (e) => {
     e.preventDefault();
   }
@@ -18,8 +24,23 @@ const Login = () => {
         background: `linear-gradient(134.56deg, rgba(155, 126, 234, 0.1) 10.8%, rgba(255, 127, 127, 0.1) 88.78%)`,
       }}>
         <h2 className='text-2xl font-bold mb-3'>Welcome back</h2>
-        <input type="email" placeholder="Type here" className="input input-bordered w-full mb-5" />
-        <input type="password" placeholder="Type here" className="input input-bordered w-full" />
+        <div className='relative mt-7 w-full border border-primary rounded-lg px-3 py-2.5 bg-[#ff7f7f05] z-0' >
+          <label htmlFor="" className='absolute -top-2.5 left-2 px-1 text-primary z-10' style={{ backgroundColor: "#ff7f7f05" }}>Email</label>
+          <div className='flex items-center w-full'>
+            <Image src={MailIcon} width={20} height={20} alt='mail icons' />
+            <input type="email" placeholder='example@gmail.com' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+          </div>
+        </div>
+
+        <div className='relative my-6 w-full border border-primary rounded-lg px-3 py-2.5 bg-[#ff7f7f05]' >
+          <label htmlFor="" className='absolute -top-3 left-2 z-10 px-1 text-primary'>Password</label>
+          <div className='flex items-center w-full'>
+            <Image src={LockIcon} width={20} height={20} alt='lock icons' />
+            <input type={showPassword ? "text" : "password"} placeholder='********' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+            <Image onClick={() => setShowPassword(!showPassword)} className='cursor-pointer' src={EyeIcon} width={20} height={20} alt='eye icons' />
+          </div>
+        </div>
+
         <p className='my-3.5 underline'>Forget your password?</p>
         <input className='btn btn-primary normal-case w-full mb-3.5' type="submit" value="Login" />
         <p>Don't have an account? <Link className='underline' href="/signup">Sign up</Link></p>
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,10 +1,18 @@
 "use client";
 
+import React, { useState } from 'react';
 import Navbar from '@/components/common/Navbar';
 import Link from 'next/link';
-import React from 'react'
+import UserIcon from "../../public/assets/icons/User.svg";
+import MailIcon from "../../public/assets/icons/email.svg";
+import LockIcon from "../../public/assets/icons/Lock.svg";
+import GiftIcon from "../../public/assets/icons/Gift.svg";
+import EyeIcon from "../../public/assets/icons/Eye.svg";
+import Image from 'next/image';
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const handleLoginSubmit = (e) => {
     e.preventDefault();
   }
@@ -18,24 +26,77 @@ const SignUp = () => {
         background: `linear-gradient(134.56deg, rgba(155, 126, 234, 0.1) 10.8%, rgba(255, 127, 127, 0.1) 88.78%)`,
       }}>
         <h4 className='text-2xl mb-3'>Get more out of your files with our search tool – sign up today</h4>
-        <div className='flex items-center justify-center'>
-          <input type="text" placeholder="First name" className="input input-bordered w-full mb-5 mr-1" />
-          <input type="text" placeholder="Last name" className="input input-bordered w-full mb-5 ml-1" />
+        <div className='flex items-center justify-center mt-7'>
+          <div className='relative w-full border border-primary rounded-lg px-3 py-2.5 mr-2 bg-[#ff7f7f05] z-0' >
+            <label htmlFor="" className='absolute -top-2.5 left-2 px-1 text-primary z-10' style={{ backgroundColor: "#ff7f7f05" }}>First name</label>
+            <div className='flex items-center w-full'>
+              <Image src={UserIcon} width={20} height={20} alt='mail icons' />
+              <input type="text" placeholder='Rakibul' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+            </div>
+          </div>
+          <div className='relative w-full border border-primary rounded-lg px-3 py-2.5 ml-2 bg-[#ff7f7f05] z-0' >
+            <label htmlFor="" className='absolute -top-2.5 left-2 px-1 text-primary z-10' style={{ backgroundColor: "#ff7f7f05" }}>Last name</label>
+            <div className='flex items-center w-full'>
+              <Image src={UserIcon} width={20} height={20} alt='mail icons' />
+              <input type="text" placeholder='Islam' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+            </div>
+          </div>
         </div>
-        <input type="text" placeholder="User name" className="input input-bordered w-full mb-5" />
-        <input type="password" placeholder="Email" className="input input-bordered w-full mb-5" />
-        <input type="password" placeholder="Password" className="input input-bordered w-full mb-5" />
+
+        <div className='relative mt-7 w-full border border-primary rounded-lg px-3 py-2.5 bg-[#ff7f7f05] z-0' >
+          <label htmlFor="" className='absolute -top-2.5 left-2 px-1 text-primary z-10' style={{ backgroundColor: "#ff7f7f05" }}>User name</label>
+          <div className='flex items-center w-full'>
+            <Image src={UserIcon} width={20} height={20} alt='mail icons' />
+            <input type="text" placeholder='rakibul-wdp' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+          </div>
+        </div>
+        <span className='text-sm'>username (must start with a letter) - (letters, numbers, and underscores)</span>
+
+        <div className='relative mt-5 w-full border border-primary rounded-lg px-3 py-2.5 bg-[#ff7f7f05] z-0' >
+          <label htmlFor="" className='absolute -top-2.5 left-2 px-1 text-primary z-10' style={{ backgroundColor: "#ff7f7f05" }}>Email</label>
+          <div className='flex items-center w-full'>
+            <Image src={MailIcon} width={20} height={20} alt='mail icons' />
+            <input type="email" placeholder='example@gmail.com' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+          </div>
+        </div>
+
+        <div className='relative mt-7 w-full border border-primary rounded-lg px-3 py-2.5 bg-[#ff7f7f05]' >
+          <label htmlFor="" className='absolute -top-3 left-2 z-10 px-1 text-primary'>Password</label>
+          <div className='flex items-center w-full'>
+            <Image src={LockIcon} width={20} height={20} alt='lock icons' />
+            <input type={showPassword ? "text" : "password"} placeholder='********' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+            <Image onClick={() => setShowPassword(!showPassword)} className='cursor-pointer' src={EyeIcon} width={20} height={20} alt='eye icons' />
+          </div>
+        </div>
+
         <p>password requirements</p>
-        <div className='flex items-center justify-between text-sm'><p>8 characters minimum Must contain one lower-case letter</p><div className="form-control">
-          <input type="checkbox" checked="checked" className="checkbox checkbox-success checkbox-xs" />
-        </div></div>
-        <div className='flex items-center justify-between text-sm'><p>one upper-case letter</p><div className="form-control">
-          <input type="checkbox" checked="checked" className="checkbox checkbox-success checkbox-xs" />
-        </div></div>
-        <div className='flex items-center justify-between text-sm mb-5'><p>one digit and one special character</p><div className="form-control">
-          <input type="checkbox" checked="checked" className="checkbox checkbox-success checkbox-xs" />
-        </div></div>
-        <input type="text" placeholder="Referral code" className="input input-bordered w-full mb-5" />
+        <div className='flex items-center justify-between text-sm'>
+          <p>8 characters minimum Must contain one lower-case letter</p>
+          <div className="form-control">
+            <input type="checkbox" defaultChecked="checked" className="checkbox checkbox-success checkbox-xs" />
+          </div>
+        </div>
+        <div className='flex items-center justify-between text-sm'>
+          <p>one upper-case letter</p>
+          <div className="form-control">
+            <input type="checkbox" defaultChecked="checked" className="checkbox checkbox-success checkbox-xs" />
+          </div>
+        </div>
+        <div className='flex items-center justify-between text-sm mb-5'>
+          <p>one digit and one special character</p>
+          <div className="form-control">
+            <input type="checkbox" defaultChecked="checked" className="checkbox checkbox-success checkbox-xs" />
+          </div>
+        </div>
+
+        <div className='relative mt-5 mb-7 w-full border border-primary rounded-lg px-3 py-2.5 bg-[#ff7f7f05] z-0' >
+          <label htmlFor="" className='absolute -top-2.5 left-2 px-1 text-primary z-10' style={{ backgroundColor: "#ff7f7f05" }}>Referral code (optional)</label>
+          <div className='flex items-center w-full'>
+            <Image src={GiftIcon} width={20} height={20} alt='mail icons' />
+            <input type="text" placeholder='Ux123' className='w-full outline-none bg-[#ff7f7f05] ml-2 text-primary' />
+          </div>
+        </div>
+
         <input className='btn btn-primary normal-case w-full mb-3' type="submit" value="Sign Up" />
         <p>Already have an account? <Link className='underline' href="/login">Login</Link></p>
       </form>
