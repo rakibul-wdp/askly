5b579ff68a4919db17b73ba47e519eb2ee9c25db
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-23 16:56:55 +0600

    responsive pp page

--- components/privacyPolicy/PrivacyPolicyContent.js
+++ components/privacyPolicy/PrivacyPolicyContent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PrivacyPolicyContent = () => {
   return (
-    <div className='h-[91vh] pt-32 ml-14'>
+    <div className='h-[91vh] pt-32 max-w-[1536px] mx-auto px-3 sm:px-5 md:px-8 lg:px-14 xl:px-20 2xl:px-24'>
       <h2 className='text-3xl font-bold mb-5'>Privacy Policy</h2>
     </div>
   )
