05e61cc86c50c1c155cbb374ca700b160a390641
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-25 18:53:25 +0600

    team member animation added

--- components/about/Team.js
+++ components/about/Team.js
@@ -1,8 +1,20 @@
+"use client"
+
 import { teamMembers } from '@/mockData/teamMembers';
 import Image from 'next/image';
-import React from 'react'
+import React, { useState } from 'react'
 
 const Team = () => {
+  const [hoveredIndex, setHoveredIndex] = useState(1);
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(1);
+  };
+
   return (
     <section className='pt-24 px-3 sm:px-5 md:px-8 lg:px-14 xl:px-20 2xl:px-24' style={{
       background: `linear-gradient(90deg, rgba(132, 33, 192, 0.2) 0%, rgba(132, 33, 192, 0.2) 100%)`,
@@ -14,17 +26,22 @@ const Team = () => {
           <p className='my-5 font-medium hidden lg:block'>Our team is committed to continuous learning and growth. We stay up-to-date with the latest technologies and industry trends, and we encourage each other to pursue personal and professional development.</p>
           <p className='font-medium hidden lg:block'>We are proud of the work we do and the impact it has on our clients and their users. Thank you for being a part of our team and for your contribution to our shared mission.</p>
         </div>
-        <div>
+        <div className='overflow-y-auto h-[65vh] scroll-hidden'>
           {
             teamMembers.map((member) => (
-              <div className='flex justify-between rounded-lg mb-5' style={{ background: "rgba(132, 33, 192, 0.1)" }} key={member.id}>
-                <Image src={member.img} className='w-[200px]' alt='team member' />
+              <div className={`flex justify-between rounded-lg mb-5 ${(member.id === 1 && hoveredIndex === member.id) ? 'opacity-100' : 'opacity-60'
+                } hover:opacity-100 duration-100 hover:shadow-md`}
+                style={{ background: "rgba(132, 33, 192, 0.1)" }}
+                key={member.id}
+                onMouseEnter={() => handleMouseEnter(member.id)}
+                onMouseLeave={handleMouseLeave}>
+                <Image src={member.img} className='min-w-[175px]' alt='team member' />
                 <div className='px-5 py-2'>
                   <div className='flex items-center justify-between mb-3'>
                     <h4 className='text-lg font-medium'>{member.name}</h4>
                     <p className='text-lg'>{member.designation}</p>
                   </div>
-                  <p className='text-sm'>{member.description}</p>
+                  <p className='text-sm sm:text-base lg:text-sm xl:text-base'>{member.description}</p>
                 </div>
               </div>
             ))
--- mockData/teamMembers.js
+++ mockData/teamMembers.js
@@ -28,5 +28,12 @@ export const teamMembers = [
     name: "Nour eldin",
     designation: "Product designer",
     description: "With over 20 years of experience in the tech industry, Nour brings a wealth of knowledge and expertise to Askly. As our Product designer, Nour is passionate about helping individuals and businesses gain valuable insights from their data."
+  },
+  {
+    id: 5,
+    img: TeamMemberImg,
+    name: "Nour eldin",
+    designation: "Product designer",
+    description: "With over 20 years of experience in the tech industry, Nour brings a wealth of knowledge and expertise to Askly. As our Product designer, Nour is passionate about helping individuals and businesses gain valuable insights from their data."
   }
 ]
