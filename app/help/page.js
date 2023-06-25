cbf1c507ed77a20cd9666c07001338da6cf1a042
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-25 21:48:42 +0600

    faq color fix

--- app/help/page.js
+++ app/help/page.js
@@ -6,7 +6,7 @@ import React from 'react'
 const Help = () => {
   return (
     <div className='mx-0' style={{
-      background: `linear-gradient(134.56deg, rgba(155, 126, 234, 0.2) 10.8%, rgba(255, 127, 127, 0.2) 88.78%)`,
+      background: `linear-gradient(134.56deg, rgba(155, 126, 234, 0.03) 10.8%, rgba(255, 127, 127, 0.2) 88.78%)`,
     }}>
       <div className='max-w-screen-2xl mx-auto px-3 sm:px-5 md:px-8 lg:px-14 xl:px-20 2xl:px-24'>
         <FAQ />
